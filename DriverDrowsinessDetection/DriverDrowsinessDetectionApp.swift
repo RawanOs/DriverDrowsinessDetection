5ceeffa43a3d76e8813d6b83b260b212930dc517
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-06-05 19:57:29 +0300

    Initial Commit

---
+++ DriverDrowsinessDetection/DriverDrowsinessDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverDrowsinessDetectionApp.swift
+//  DriverDrowsinessDetection
+//
+//  Created by Rawan on 16/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DriverDrowsinessDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
